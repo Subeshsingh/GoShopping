872e09ad0764ed9e0d503a426a98f6eab3169b5f
Subeshsingh <subeshbikkisingh@gmail.com>
2020-03-16 00:58:22 +0530

    second after adding auth and redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import Auth from './containers/auth/Auth';
+
+import {Route , Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
+  return ( 
+    <Fragment>
+      <p> Hello I am Main Page Of this Application change Link to /login fro Login Page</p>
+      <Switch>
+        <Route path='/login' exact component={Auth}/>
+      </Switch>
+    </Fragment>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'; 
+import authReducer from './store/reducers/auth';
+
+
+const composeEnhancers = process.env.NODE_ENV ==='development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const rootReducer = combineReducers({
+    auth: authReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+let app=(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
